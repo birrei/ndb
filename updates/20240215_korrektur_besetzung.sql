4493ad1a377bd169ad6cf445ba27822c4802bb33
birgitreiner <birgitreiner@outlook.de>
2024-02-15 15:52:47 +0100

    Korrektur Besetzungs-Eintrag

---
+++ updates/20240215_korrektur_besetzung.sql
@@ -0,0 +1,25 @@
+/*
+prod: 
+
+6	Symphonie-Orchester
+umbiegen zu
+7	Sinfonieorchester
+
+*/
+
+-- select * from musikstueck_besetzung where BesetzungID=6
+-- ID	MusikstueckID	BesetzungID
+-- 237	65	6
+
+-- update musikstueck_besetzung set BesetzungID=7 where BesetzungID=6
+
+-- delete from besetzung where ID=6
+
+-- Musikstück-Zuordnungen mit besetzung "Sinfonieorchester"
+--- select * from musikstueck_besetzung where BesetzungID=7
+
+-- ID	MusikstueckID	BesetzungID
+-- 237	65	7
+-- 238	96	7
+-- 239	98	7
+-- 385	102	7
