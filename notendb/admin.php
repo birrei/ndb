cdd2b713ef4ff2007cdf0076be001edbb7cc175d
birgitreiner <birgitreiner@outlook.de>
2024-02-22 14:06:56 +0100

    Admin-Seite, DDL-script Ausführung

---
+++ notendb/admin.php
@@ -0,0 +1,12 @@
+<?php 
+include('head.php');
+?>
+
+<p><a href="list_tables.php">list_tables.php</a></p> 
+<p><a href="sqlexec.php">sqlexec.php</a> </p> 
+<p><a href="sqlexec_ddl.php?script=ddl_views">sqlexec_ddl.php: Views anlegen</a> </p> 
+
+<?php 
+include('foot.php');
+?>
+
--- notendb/sqlexec_ddl.php
+++ notendb/sqlexec_ddl.php
@@ -6,13 +6,15 @@ Die SQL-commands müssen durch ; voneinander getrennt sein.
 include('head.php');
 include("dbconnect.php"); // nur wenn benötigt 
 
-// $file_name='ddl_tables.sql'; // initiale installation 
-$file_name='ddl_views.sql'; //  
+$file_name=$_GET['script'].'.sql';
+
+$sql=''; 
 $sqltext = file_get_contents($file_name, true);
 $cmds = explode(';', $sqltext);
 foreach($cmds as $cmd){
   if (mysqli_query( $db, $cmd)) {
-     echo '<p>sql wurde ausgeführt: <br />'.$cmd; 
+    $sql= trim($cmd); 
+     echo '<p>sql wurde ausgeführt: <br /><pre>'.$sql.'</pre>'; 
   }
   else 
   {
