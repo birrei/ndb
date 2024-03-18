d24c5cd947ed3112983ae684cc6fcbb568650704
birgitreiner <birgitreiner@outlook.de>
2024-03-18 18:16:27 +0100

    auf Verwendung cl_db.php umgesetllt

--- notendb/sqlexec_ddl.php
+++ notendb/sqlexec_ddl.php
@@ -4,23 +4,45 @@ Führt alle in ddl.sql enthaltenen SQLS aus.
 Die SQL-commands müssen durch ; voneinander getrennt sein. 
 */ 
 include('head.php');
-include("dbconnect.php"); // nur wenn benötigt 
+include("cl_db.php"); 
+
 
 $file_name=$_GET['script'].'.sql';
 
 $sql=''; 
 $sqltext = file_get_contents($file_name, true);
 $cmds = explode(';', $sqltext);
+
+$conn = new DbConn(); 
+$db=$conn->db; 
+
 foreach($cmds as $cmd){
   $sql= trim($cmd); 
-  echo '<p>sql wird ausgeführt: <br /><pre>'.$sql.'</pre>'; 
-  if (mysqli_query( $db, $sql)) {
-    echo '<p>OK</p>';  
+  $stmt = $db->prepare($sql); 
+  echo '<p>/********************************************/<br />'; 
+
+  try {    
+    $stmt->execute(); 
+    echo '<p>sql wurde ausgeführt: <br /><pre>'.$sql.'</pre>'; 
+
   }
-  else  {
-    echo '<p>Fehler: <br />'.pmysqli_error(); 
-  } 
+  catch (PDOException $e) {
+    include_once("cl_html_info.php"); 
+    $info = new HtmlInfo();      
+    $info->print_user_error(); 
+    $info->print_error($stmt, $e); 
+  }
+
 }
+
+
+//   if (mysqli_query( $db, $sql)) {
+//     echo '<p>OK</p>';  
+//   }
+//   else  {
+//     echo '<p>Fehler: <br />'.pmysqli_error(); 
+//   } 
+// }
 
 
 include('foot.php');
