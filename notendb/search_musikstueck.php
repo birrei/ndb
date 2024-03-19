81c122df39cf018654838d47866f112f44e6ba08
birgitreiner <birgitreiner@outlook.de>
2024-03-19 19:51:30 +0100

    Suchformular ergänzende Funktionen

--- notendb/search_musikstueck.php
+++ notendb/search_musikstueck.php
@@ -12,10 +12,15 @@ include("cl_verwendungszweck.php");
 include("cl_db.php");
 
 
-$table='besetzung'; 
 
 $Besetzungen=[]; 
 $Verwendungszwecke=[]; 
+
+if (isset($_POST['Ebene'])) {
+  $Ebene=$_POST["Ebene"]; 
+} else {
+  $Ebene='Musikstueck'; // default 
+}
 
 if ("POST" == $_SERVER["REQUEST_METHOD"]) {
   if (isset($_REQUEST['Besetzungen'])) {
@@ -48,9 +53,19 @@ if ("POST" == $_SERVER["REQUEST_METHOD"]) {
   </tr> 
 
 </table> 
-<hr />
 
-<td class="eingabe"><input type="submit" value="Suchen"></td>
+<fieldset>Aggegationsebene: 
+    <input type="radio" id="sm" name="Ebene" value="Sammlung" <?php echo ($Ebene=='Sammlung'?'checked':'') ?>>
+    <label for="sm">Sammlung</label> 
+    <input type="radio" id="mu" name="Ebene" value="Musikstueck" <?php echo ($Ebene=='Musikstueck'?'checked':'') ?>> 
+    <label for="mi">Musikstück</label>
+    <input type="radio" id="st" name="Ebene" value="Satz" <?php echo ($Ebene=='Satz'?'checked':'') ?>>
+    <label for="st">Satz</label> 
+  </fieldset>
+
+
+<p> <input type="submit" value="Suchen"></p> 
+
 </form>
 
 
@@ -73,16 +88,54 @@ if ("POST" == $_SERVER["REQUEST_METHOD"]) {
     }
   }
 
-  if ($filter ) {
+  if (isset($_POST['Ebene'])) {
+    $Ebene=$_POST["Ebene"]; 
+  }
+  
+  
 
-    $query="SELECT s.ID
-        ,s.Name as Sammlung
-        , st.Name as Standort 
-        , m.Nummer as MNr
-        , m.Name as Musikstueck
-        , GROUP_CONCAT(DISTINCT b.Name order by b.Name SEPARATOR ', ') Besetzungen
-        , GROUP_CONCAT(DISTINCT v.Name order by v.Name SEPARATOR ', ') Verwendungszwecke   
-        , GROUP_CONCAT(DISTINCT sa.Name order by sa.Name SEPARATOR ', ') Saetze                 
+  if ($filter ) {
+    $query=""; 
+
+    /* Spaltenauswahl und Aggregation abhängig von Ebene */ 
+    switch ($Ebene){
+      case 'Sammlung': 
+        $query.="SELECT s.ID
+            ,s.Name as Sammlung
+            , st.Name as Standort
+            , GROUP_CONCAT(DISTINCT m.Name order by m.Name SEPARATOR ', ') Musikstuecke
+            , GROUP_CONCAT(DISTINCT b.Name order by b.Name SEPARATOR ', ') Besetzungen
+            , GROUP_CONCAT(DISTINCT v.Name order by v.Name SEPARATOR ', ') Verwendungszwecke   
+            ";
+
+        break; 
+      case 'Musikstueck': 
+        $query.="SELECT s.ID
+            ,s.Name as Sammlung
+            , st.Name as Standort
+            , m.Nummer as MNr
+            , m.Name as Musikstueck
+            , GROUP_CONCAT(DISTINCT b.Name order by b.Name SEPARATOR ', ') Besetzungen
+            , GROUP_CONCAT(DISTINCT v.Name order by v.Name SEPARATOR ', ') Verwendungszwecke   
+            , GROUP_CONCAT(DISTINCT sa.Name order by sa.Name SEPARATOR ', ') Saetze                 
+            ";         
+        break; 
+      case 'Satz': 
+        $query.="SELECT s.ID
+            ,s.Name as Sammlung
+            , st.Name as Standort
+            , m.Nummer as MNr
+            , m.Name as Musikstueck
+            , sa.Name as Satz 
+            , GROUP_CONCAT(DISTINCT b.Name order by b.Name SEPARATOR ', ') Besetzungen
+            , GROUP_CONCAT(DISTINCT v.Name order by v.Name SEPARATOR ', ') Verwendungszwecke                  
+            ";            
+        break;      
+
+    }
+
+
+    $query.="
       FROM musikstueck m
       LEFT JOIN sammlung s on s.ID = m.SammlungID 
       LEFT JOIN standort st on s.StandortID = st.ID 
@@ -90,25 +143,42 @@ if ("POST" == $_SERVER["REQUEST_METHOD"]) {
       LEFT JOIN besetzung b on mb.BesetzungID = b.ID
       LEFT JOIN musikstueck_verwendungszweck mv on m.ID = mv.MusikstueckID 
       LEFT JOIN verwendungszweck v on mv.VerwendungszweckID=v.ID    
-      /* */
       LEFT JOIN satz sa on sa.MusikstueckID = m.ID 
-
-
-
       WHERE 1=1 
-      "; 
+      ". PHP_EOL; 
 
       if($filterBesetzung!=''){
-        $query.=' AND mb.BesetzungID '.$filterBesetzung; 
+        $query.=' AND mb.BesetzungID '.$filterBesetzung. PHP_EOL; 
       }
       if($filterVerwendungszweck!=''){
-        $query.=' AND mv.VerwendungszweckID '.$filterVerwendungszweck; 
-      }            
-      $query.= '
-        group by 
-            m.ID  
-        ORDER BY
-            s.Name, m.Nummer'; 
+        $query.=' AND mv.VerwendungszweckID '.$filterVerwendungszweck. PHP_EOL; 
+      }
+
+      /* Gruppierung abhängig von Ebene  */
+      switch ($Ebene){    
+        case 'Sammlung': 
+          $query.=" group by s.ID". PHP_EOL;     
+          break; 
+        case 'Musikstueck': 
+          $query.=" group by m.ID". PHP_EOL;         
+          break; 
+        case 'Satz': 
+          $query.=" group by sa.ID". PHP_EOL;             
+          break;      
+      }
+
+      /* Sortierung abhängig von Ebene  */
+      switch ($Ebene){    
+        case 'Sammlung': 
+          $query.=" ORDER BY s.Name". PHP_EOL;     
+          break; 
+        case 'Musikstueck': 
+          $query.=" ORDER BY s.Name, m.Nummer". PHP_EOL;         
+          break; 
+        case 'Satz': 
+          $query.=" ORDER BY s.Name, m.Nummer, sa.Nr". PHP_EOL;           
+          break;      
+      }
 
       // echo '<pre>'.$query.'</pre>'; // Test 
 
@@ -135,7 +205,7 @@ if ("POST" == $_SERVER["REQUEST_METHOD"]) {
     }
     else {
           echo '<p>Es wurde kein Filter gesetzt. </p>'; 
-      }
+  }
 
 include('foot.php');
 
