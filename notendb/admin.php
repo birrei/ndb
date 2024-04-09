7d9b2633725945e2aa7bd6de683c518ab4d4911c
birgitreiner <birgitreiner@outlook.de>
2024-04-09 19:06:07 +0200

    Ergänzung Change-Log Pfad

--- notendb/admin.php
+++ notendb/admin.php
@@ -1,8 +1,8 @@
 <?php 
 include('head.php');
 ?>
-
 <h2>Helpers</h2>
+
 <p><a href="sqlexec.php">SQL Query-Box</a> </p> 
 <p><a href="list_tables.php">list_tables.php</a></p> 
 
@@ -10,6 +10,11 @@ include('head.php');
 <p><a href="sqlexec_ddl.php?script=ddl_views">ddl_views.sql</a></p> 
 <p><a href="sqlexec_ddl.php?script=ddl_views_v_musikstueck">ddl_views_v_musikstueck.sql<a> </p> 
 <p><a href="sqlexec_ddl.php?script=ddl_views_v_satz">ddl_views_v_satz.sql<a> </p> 
+
+
+<h2>Changelog</h2>
+
+<p> a href="https://github.com/birrei/ndb/blob/main/notendb/changelog.md">https://github.com/birrei/ndb/blob/main/notendb/changelog.md</a></p>
 
 <?php 
 include('foot.php');
