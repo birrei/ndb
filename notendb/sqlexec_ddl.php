59213cecdd54fefc17e1cd6edfa26a7d3f45e07f
birgitreiner <birgitreiner@outlook.de>
2024-04-18 19:48:31 +0200

    improve sql-execute - Script

--- notendb/sqlexec_ddl.php
+++ notendb/sqlexec_ddl.php
@@ -1,37 +1,50 @@
 <?php 
-/* 
-Führt alle in ddl.sql enthaltenen SQLS aus. 
-Die SQL-commands müssen durch ; voneinander getrennt sein. 
-*/ 
 include('head.php');
 include("cl_db.php"); 
 
+/* 
+Script führt die SQL-Commands aus allen *.sql-Dateien (die im gleichen Ordner liegen) aus. 
+Mehrere SQL-Commands in einer Datei müssen durch ein Semikolon ";" getrennt sein. 
+*/
 
-$file_name=$_GET['script'].'.sql';
+$dir='.'; 
 
-$sql=''; 
-$sqltext = file_get_contents($file_name, true);
-$cmds = explode(';', $sqltext);
+if ( is_dir ( $dir )){
+  if ( $handle = opendir($dir) )
+  {
+    while (($file = readdir($handle)) !== false)
+    {
+      if (pathinfo($file)['extension']=='sql') {
+        echo '<p>Datei: '.$file.'</p>'; 
 
-$conn = new DbConn(); 
-$db=$conn->db; 
+        $sqltext = file_get_contents($file, true);
+        $cmds = explode(';', $sqltext);
 
-foreach($cmds as $cmd){
-  $sql= trim($cmd); 
-  echo '<p>SQL:<br /><pre>'.$sql.'</pre></p>'; 
-  $stmt = $db->prepare($sql); 
-  try {    
-    $stmt->execute(); 
+        $conn = new DbConn(); 
+        $db=$conn->db; 
+
+        foreach($cmds as $cmd){
+          $sql= trim($cmd); 
+          echo '<p>SQL:<br /><pre>'.$sql.'</pre></p>'; 
+          $stmt = $db->prepare($sql); 
+          try {    
+            $stmt->execute(); 
+            echo '<p>SQL wurde erfolgreich ausgeführt.</p>';
+          }
+          catch (PDOException $e) {
+            include_once("cl_html_info.php"); 
+            $info = new HtmlInfo();      
+            $info->print_user_error(); 
+            $info->print_error($stmt, $e); 
+          }
+          echo '<p>/********************************************/<br />'; 
+        }
+      }
+    }
+    closedir($handle);
   }
-  catch (PDOException $e) {
-    include_once("cl_html_info.php"); 
-    $info = new HtmlInfo();      
-    $info->print_user_error(); 
-    $info->print_error($stmt, $e); 
-  }
-  echo '<p>/********************************************/<br />'; 
-
 }
+echo "</ol>";
 
 include('foot.php');
 
