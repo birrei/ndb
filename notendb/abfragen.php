6268ce9e058ed1e9f44667c660766dd8761fda21
birgitreiner <birgitreiner@outlook.de>
2024-06-07 23:36:01 +0200

    Parameter Name geändert

--- notendb/abfragen.php
+++ notendb/abfragen.php
@@ -2,52 +2,44 @@
 <?php 
 include('head.php');
 
-
-$tests[] = array('name' => 'Sammlungen ohne Musikstück'
+$abfragen[] = array('name' => 'Sammlungen ohne Musikstück'
               , 'testview'   => "v_test_sammlung_ohne_musikstueck"
               , 'table'   => "sammlung"              
               );
 
-$tests[] = array('name' => 'Musikstücke ohne Satz'
+$abfragen[] = array('name' => 'Musikstücke ohne Satz'
             , 'testview'   => "v_test_musikstueck_ohne_satz"
             , 'table'   => "musikstueck"              
            );
 
-$tests[] = array('name' => 'Musikstücke ohne Besetzung'
+$abfragen[] = array('name' => 'Musikstücke ohne Besetzung'
            , 'testview'   => "v_test_musikstueck_ohne_besetzung"
            , 'table'   => "musikstueck"              
           );
 
-$tests[] = array('name' => 'Satz ohne Spieldauer'
+$abfragen[] = array('name' => 'Satz ohne Spieldauer'
           , 'testview'   => "v_test_satz_ohne_spieldauer"
           , 'table'   => "satz"              
          );
 
-$tests[] = array('name' => 'Satz ohne Erprobt-Angabe'
+$abfragen[] = array('name' => 'Satz ohne Erprobt-Angabe'
          , 'testview'   => "v_test_satz_ohne_erprobt"
          , 'table'   => "satz"              
         );
 
-$tests[] = array('name' => 'Satz ohne Schwierigkeitsgrad'
+$abfragen[] = array('name' => 'Satz ohne Schwierigkeitsgrad'
               , 'testview'   => "v_test_satz_ohne_schwierigkeitsgrad"
               , 'table'   => "satz"              
               );
 
 
-                 
-
-          
-
-           
-
-
 
 echo '<h2>Tests</h2>'; 
 
-foreach ($tests as $test)
+foreach ($abfragen as $abfrage)
 {
 
-  $query='SELECT * FROM '.$test["testview"]; 
+  $query='SELECT * FROM '.$abfrage["testview"]; 
 
   include_once("cl_db.php");
   $conn = new DbConn(); 
@@ -58,9 +50,10 @@ foreach ($tests as $test)
   try {
     $select->execute(); 
     include_once("cl_html_table.php");      
-    echo '<h3>'.$test['name'].'</h3>';     
+    echo '<h3>'.$abfrage['name'].'</h3>';     
     $html = new HtmlTable($select); 
-    $html->print_table($test['table'] , true); 
+    $html->print_table($abfrage['table'] , true,'', ucfirst($abfrage['table'])); 
+  
     
   }
   catch (PDOException $e) {
