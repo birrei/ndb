1fe78bf6ff6b10a498bb3075eb822c4f319e38ac
birgitreiner <birgitreiner@outlook.de>
2024-06-10 18:49:24 +0200

    Umbenannt

--- sql/queries/satz_lookup.sql
+++ sql/queries/satz_lookup.sql
@@ -1,19 +1,29 @@
 
-/* satz und Lookups */
+/* satz und dazugehörige Lookups */
+
+    select satz.ID
+    , satz.Name
+    FROM 
+        satz 
+        left join satz_lookup on satz_lookup.SatzID = satz.ID 
+        left join lookup on lookup.ID = satz_lookup.LookupID 
+        left join lookup_type on lookup_type.ID = lookup.LookupTypeID
+    -- where satz_lookup.SatzID is not nULL 
+    group by satz.ID 
 
 
-select satz.ID
-, satz.Name
+/* satz mit lookups */ 
 
-FROM 
-    satz 
+    select satz.ID
+        , satz.Name as SatzName 
+        , lookup.Name as LookupName 
+        , lookup_type.Name as TypeName 
+    FROM 
+        satz 
+        left join satz_lookup on satz_lookup.SatzID = satz.ID 
+        left join lookup on lookup.ID = satz_lookup.LookupID 
+        left join lookup_type on lookup_type.ID = lookup.LookupTypeID
+    where lookup.LookupTypeID =4
 
-    left join satz_lookup on satz_lookup.SatzID = satz.ID 
-    left join lookup on lookup.ID = satz_lookup.LookupID 
-    left join lookup_type on lookup_type.ID = lookup.LookupTypeID
 
-group by satz.ID 
-
-
-        
 
