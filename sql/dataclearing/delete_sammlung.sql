a69054ecbe09f958930458138f711f7a6c3c5afe
birgitreiner <birgitreiner@outlook.de>
2024-06-16 22:12:33 +0200

    Dataclearing-Script "Sammlung Löschen"

--- sql/dataclearing/delete_sammlung.sql
+++ sql/dataclearing/delete_sammlung.sql
@@ -1,11 +1,12 @@
 /****** Eine Sammlung mit allen Untereinheiten löschen    *******/
-/* Stand Mai 2024, Script ggf. noch ergänzen! */ 
+/* Stand Mai 2024, ggf. noch ergänzen! */ 
 
+/* Satz und Verknüpfungen */
 delete from satz_notenwert USING 
 satz_notenwert
 inner join satz on satz.ID= satz_notenwert.SatzID
 inner join musikstueck on musikstueck.ID = satz.MusikstueckID
-where musikstueck.SammlungID = 38
+where musikstueck.SammlungID = 149
 ; 
 
 delete from 
@@ -15,32 +16,32 @@ inner join satz
     on satz.ID= satz_strichart.SatzID
 inner join musikstueck 
     on musikstueck.ID = satz.MusikstueckID
-where musikstueck.SammlungID = 38
+where musikstueck.SammlungID = 149
 ; 
 
 delete from 
 satz USING 
 satz inner join musikstueck 
     on musikstueck.ID = satz.MusikstueckID
-where musikstueck.SammlungID = 38
+where musikstueck.SammlungID = 149
 ; 
 
 delete from 
 musikstueck_besetzung USING 
 musikstueck_besetzung inner join musikstueck 
     on musikstueck.ID = musikstueck_besetzung.MusikstueckID
-where musikstueck.SammlungID = 38
+where musikstueck.SammlungID = 149
 ;
 
 delete from 
 musikstueck_verwendungszweck USING 
 musikstueck_verwendungszweck inner join musikstueck 
     on musikstueck.ID = musikstueck_verwendungszweck.MusikstueckID
-where musikstueck.SammlungID = 38
+where musikstueck.SammlungID = 149
 ;
 
-delete from musikstueck where SammlungID = 38
+delete from musikstueck where SammlungID = 149
 ;
 
-delete from sammlung where ID = 38
+delete from sammlung where ID = 149
 ;
