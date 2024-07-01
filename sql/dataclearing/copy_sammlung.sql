8cea090c89e2b2fbb687f0df732c7d9ea4a755ce
birgitreiner <birgitreiner@outlook.de>
2024-07-02 00:54:14 +0200

    Data-Clearing Script

---
+++ sql/dataclearing/copy_sammlung.sql
@@ -0,0 +1,90 @@
+
+/*
+Ein Musikstück (inklusive Sätze) inneralb einer Sammlung duplizieren 
+*/ 
+
+-- SELECT * 
+-- from musikstueck 
+-- where SammlungID = 113; 
+
+
+insert into sammlung (Name, VerlagID, StandortID, Bemerkung)
+SELECT Name, VerlagID, StandortID, Bemerkung
+from sammlung 
+where ID= 162
+
+
+
+
+insert into musikstueck (
+    Name
+    , Opus
+    , SammlungID
+    , Nummer
+    , KomponistID
+    , Bearbeiter
+    , JahrAuffuehrung
+    , GattungID
+    , EpocheID
+)
+   
+select 
+Name
+, Opus
+, 170 as SammlungID
+, Nummer
+, KomponistID
+, Bearbeiter
+, JahrAuffuehrung
+, GattungID
+, EpocheID
+from musikstueck 
+where SammlungID=162
+; 
+
+
+
+/****************************/
+
+insert into satz 
+(
+    MusikstueckID
+, Name
+, Tonart
+, Taktart
+, Tempobezeichnung
+, Spieldauer
+, Lagen
+, Bemerkung
+, Nr
+, Notenwerte
+, ErprobtID
+, SchwierigkeitsgradID
+, BesonderheitID
+
+)
+select 
+     318 as MusikstueckID
+    , Name
+    , Tonart
+    , Taktart
+    , Tempobezeichnung
+    , Spieldauer
+    , Lagen
+    , Bemerkung
+    , Nr
+    , Notenwerte
+    , ErprobtID
+    , SchwierigkeitsgradID
+    , BesonderheitID
+from satz 
+where MusikstueckID =296
+
+
+
+
+/* 
+Achtung FElder mit Mehrfach-Zuordnung müssen - sofern sie befüllt sind - extra kopiert werden! 
+
+*/
+
