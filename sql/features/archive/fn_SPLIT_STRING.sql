e54c6ba8a329e8c8158bf345caeab2c303c78ae2
birgitreiner <birgitreiner@outlook.de>
2024-07-06 10:25:08 +0200

    aufrgeräumt

--- sql/features/archive/fn_SPLIT_STRING.sql
+++ sql/features/archive/fn_SPLIT_STRING.sql
@@ -1,3 +1,4 @@
+/* nur fuer Entwicklung verwendet, fuer Anwendung nicht notwendig */ 
 CREATE FUNCTION fn_SPLIT_STRING(str VARCHAR(255), delim VARCHAR(12), pos INT)
 RETURNS VARCHAR(255)
 RETURN TRIM(REPLACE(SUBSTRING(SUBSTRING_INDEX(str, delim, pos),
