eabd454125a215e618436438741194448155b3d2
birgitreiner <birgitreiner@outlook.de>
2024-07-08 10:24:43 +0200

    Umstellung Abfrage

--- notendb/ddl/v_test_satz_ohne_schwierigkeitsgrad.sql
+++ notendb/ddl/v_test_satz_ohne_schwierigkeitsgrad.sql
@@ -1,6 +1,6 @@
 
 CREATE  OR REPLACE VIEW v_test_satz_ohne_schwierigkeitsgrad AS 
-        select s.Name as Sammlung_Name
+select s.Name as Sammlung_Name
         , m.Name as Musikstueck_Name
         , sa.Nr 
         , sa.Name as Satz_Name 
@@ -8,6 +8,6 @@ CREATE  OR REPLACE VIEW v_test_satz_ohne_schwierigkeitsgrad AS
 from musikstueck m 
     inner join  sammlung s on s.ID = m.SammlungID 
     inner join satz sa on sa.MusikstueckID = m.ID 
-    left join schwierigkeitsgrad on schwierigkeitsgrad.ID = sa.SchwierigkeitsgradID 
- where schwierigkeitsgrad.ID is NULL
-order by sa.ID DESC 
+    left join satz_schwierigkeitsgrad on satz_schwierigkeitsgrad.SatzID = sa.ID 
+where satz_schwierigkeitsgrad.ID is NULL
+order by sa.ID DESC
