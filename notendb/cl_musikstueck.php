658319fc17c7ebf0038cbd9ba935f1937ea0b4ea
birgitreiner <birgitreiner@outlook.de>
2024-07-09 18:57:46 +0200

    Auflistung Sätze - Abfrage anpassen

--- notendb/cl_musikstueck.php
+++ notendb/cl_musikstueck.php
@@ -316,9 +316,10 @@ class Musikstueck {
               , GROUP_CONCAT(DISTINCT concat(schwierigkeitsgrad.Name, ' - ', instrument.Name)  order by schwierigkeitsgrad.Name SEPARATOR ', ') `Schwierigkeitsgrade`  
               , erprobt.Name as Erprobt              
               , satz.Lagen
-              , GROUP_CONCAT(DISTINCT strichart.Name order by strichart.Name SEPARATOR ', ') `Stricharten`              
-              , GROUP_CONCAT(DISTINCT notenwert.Name order by notenwert.Name SEPARATOR ', ') `Notenwerte`
-              , GROUP_CONCAT(DISTINCT concat(lookup_type.Name, ': ', lookup.Name)  order by  concat(lookup_type.Name, ': ', lookup.Name)  SEPARATOR ', ') `Besonderheiten`            
+              -- , GROUP_CONCAT(DISTINCT strichart.Name order by strichart.Name SEPARATOR ', ') `Stricharten`              
+              -- , GROUP_CONCAT(DISTINCT notenwert.Name order by notenwert.Name SEPARATOR ', ') `Notenwerte`
+              -- , GROUP_CONCAT(DISTINCT concat(lookup_type.Name, ': ', lookup.Name)  order by  concat(lookup_type.Name, ': ', lookup.Name)  SEPARATOR ', ') `Besonderheiten`            
+              , v_satz_lookuptypes.LookupList as Besonderheiten              
               , satz.Bemerkung               
             from satz 
             left join satz_strichart on satz_strichart.SatzID = satz.ID 
@@ -331,9 +332,10 @@ class Musikstueck {
             LEFT JOIN schwierigkeitsgrad on schwierigkeitsgrad.ID = satz_schwierigkeitsgrad.SchwierigkeitsgradID 
             LEFT JOIN instrument on instrument.ID = satz_schwierigkeitsgrad.InstrumentID 
 
-            left join satz_lookup on satz_lookup.SatzID = satz.ID 
-            left join lookup on lookup.ID = satz_lookup.LookupID 
-            left join lookup_type on lookup_type.ID = lookup.LookupTypeID
+            -- left join satz_lookup on satz_lookup.SatzID = satz.ID 
+            -- left join lookup on lookup.ID = satz_lookup.LookupID 
+            -- left join lookup_type on lookup_type.ID = lookup.LookupTypeID
+            left join v_satz_lookuptypes on v_satz_lookuptypes.SatzID = satz.ID 
 
 
             WHERE satz.MusikstueckID = :MusikstueckID 
