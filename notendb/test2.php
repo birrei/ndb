68ff94f5ae409edd34df95e657fc96f91d986f0b
birgitreiner <birgitreiner@outlook.de>
2024-07-17 18:01:44 +0200

    Test

--- notendb/test2.php
+++ notendb/test2.php
@@ -1,32 +1,13 @@
 <?php 
 
 include('head.php');
+include('cl_html_info.php');
+
+$info=new HtmlInfo(); 
+
+$info->print_link_edit('komponist',3,'Komponist','',false);
+
 ?>
-
-<h1> Notendatenbank   </h1> 
-
-
-<div class="start"> 
-
-
-
-
-    <div class="start-box5"> 
-
-        <h2> Sonstiges </h2>
-        <table class="start">
-        <tr>
-            <td class="start"><b>Abfragen</b> (BETA) </td>
-            <td class="start"><a href="show_table2.php?table=v_abfrage&sortcol=Name&title=Abfrage&add_link_show" target="_blank">Daten anzeigen</a></td>
-            <td class="start">
-            <a href="insert_abfrage.php?title=Abfrage" target="_blank">Neu erfassen</a>
-            </td>
-        </tr>
-        </table>
-
-    </div>
-
-</div> 
 
 <?php 
 include('foot.php');
