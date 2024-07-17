bf8b8791e4edba0b7fdbcf8171368b11de0c6b19
birgitreiner <birgitreiner@outlook.de>
2024-07-17 17:48:06 +0200

    Obsolete Tabellen aus Abfrage entfernt

--- notendb/cl_musikstueck.php
+++ notendb/cl_musikstueck.php
@@ -308,41 +308,26 @@ class Musikstueck {
               , satz.Tonart
               , satz.Taktart
               , satz.Tempobezeichnung `Tempo-Bez.`
-             -- , satz.Spieldauer 
               , concat(
-                satz.Spieldauer DIV 60
-                ,''''
-                , 
-                satz.Spieldauer MOD 60
-                , ''''''
+                  satz.Spieldauer DIV 60
+                  ,''''
+                  , 
+                  satz.Spieldauer MOD 60
+                  , ''''''
                 ) as Spieldauer                    
               , GROUP_CONCAT(DISTINCT concat(schwierigkeitsgrad.Name, ' - ', instrument.Name)  order by schwierigkeitsgrad.Name SEPARATOR ', ') `Schwierigkeitsgrade`  
               , erprobt.Name as Erprobt              
               , satz.Lagen
-              -- , GROUP_CONCAT(DISTINCT strichart.Name order by strichart.Name SEPARATOR ', ') `Stricharten`              
-              -- , GROUP_CONCAT(DISTINCT notenwert.Name order by notenwert.Name SEPARATOR ', ') `Notenwerte`
-              -- , GROUP_CONCAT(DISTINCT concat(lookup_type.Name, ': ', lookup.Name)  order by  concat(lookup_type.Name, ': ', lookup.Name)  SEPARATOR ', ') `Besonderheiten`            
               , v_satz_lookuptypes.LookupList as Besonderheiten              
               , satz.Bemerkung               
-            from satz 
-            left join satz_strichart on satz_strichart.SatzID = satz.ID 
-            left join strichart on strichart.ID = satz_strichart.StrichartID
-            left join satz_notenwert on satz_notenwert.SatzID = satz.ID  
-            Left JOIN notenwert on notenwert.ID = satz_notenwert.NotenwertID
-            left JOIN erprobt on erprobt.ID = satz.ErprobtID
-
-            left JOIN satz_schwierigkeitsgrad on satz_schwierigkeitsgrad.SatzID = satz.ID 
-            LEFT JOIN schwierigkeitsgrad on schwierigkeitsgrad.ID = satz_schwierigkeitsgrad.SchwierigkeitsgradID 
-            LEFT JOIN instrument on instrument.ID = satz_schwierigkeitsgrad.InstrumentID 
-
-            -- left join satz_lookup on satz_lookup.SatzID = satz.ID 
-            -- left join lookup on lookup.ID = satz_lookup.LookupID 
-            -- left join lookup_type on lookup_type.ID = lookup.LookupTypeID
-            left join v_satz_lookuptypes on v_satz_lookuptypes.SatzID = satz.ID 
-
-
+            FrOM satz 
+              left JOIN erprobt on erprobt.ID = satz.ErprobtID
+              left JOIN satz_schwierigkeitsgrad on satz_schwierigkeitsgrad.SatzID = satz.ID 
+              LEFT JOIN schwierigkeitsgrad on schwierigkeitsgrad.ID = satz_schwierigkeitsgrad.SchwierigkeitsgradID 
+              LEFT JOIN instrument on instrument.ID = satz_schwierigkeitsgrad.InstrumentID 
+              left join v_satz_lookuptypes on v_satz_lookuptypes.SatzID = satz.ID 
             WHERE satz.MusikstueckID = :MusikstueckID 
-            group by satz.ID 
+            GROUP by satz.ID 
             ORDER by Nr"; 
                 
     include_once("cl_db.php");
