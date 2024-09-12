41fd1680f1b9c999f468e9121dd4bdd4034f0c40
birgitreiner <birgitreiner@outlook.de>
2024-09-12 13:22:19 +0200

    neue View v_satz_instrumente

---
+++ notendb/ddl/v_satz_instrumente.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE view v_satz_instrumente as  
+
+select satz_schwierigkeitsgrad.SatzID 
+        , GROUP_CONCAT(DISTINCT instrument.Name  order by instrument.Name SEPARATOR ', ') InstrumentList          
+from satz_schwierigkeitsgrad 
+    on instrument.ID = satz_schwierigkeitsgrad.InstrumentID 
+group by satz_schwierigkeitsgrad.SatzID
