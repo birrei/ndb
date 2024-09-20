378dca2e417c61b0357c873eeba79cbb128f1a8c
birgitreiner <birgitreiner@outlook.de>
2024-09-20 11:18:30 +0200

    Korrektur

--- notendb/ddl/v_satz_instrumente.sql
+++ notendb/ddl/v_satz_instrumente.sql
@@ -3,5 +3,6 @@ CREATE OR REPLACE view v_satz_instrumente as
 select satz_schwierigkeitsgrad.SatzID 
         , GROUP_CONCAT(DISTINCT instrument.Name  order by instrument.Name SEPARATOR ', ') InstrumentList          
 from satz_schwierigkeitsgrad 
+    left join instrument  
     on instrument.ID = satz_schwierigkeitsgrad.InstrumentID 
 group by satz_schwierigkeitsgrad.SatzID
