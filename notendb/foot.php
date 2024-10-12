93ec28002920f5099b9f4d84f7632ac43044ba50
birgitreiner <birgitreiner@outlook.de>
2024-10-12 12:37:45 +0200

    Menü-Links mit tabindex="-1"

--- notendb/foot.php
+++ notendb/foot.php
@@ -3,10 +3,10 @@
 <hr>
 <p>
     
-    <a href="index.php?title=Start">Startseite</a> | 
-    <a href="suche.php?title=Suche">Suche</a> | 
-    <a href="help.php?title=Hilfe">Hilfe</a>  |
-    <a href="tests.php?title=Tests">Tests</a>
+    <a href="index.php?title=Start" tabindex="-1">Startseite</a> | 
+    <a href="suche.php?title=Suche" tabindex="-1">Suche</a> | 
+    <a href="help.php?title=Hilfe" tabindex="-1">Hilfe</a>  |
+    <a href="tests.php?title=Tests" tabindex="-1">Tests</a>
 
 </p>  
 <hr>
--- notendb/head.php
+++ notendb/head.php
@@ -63,12 +63,11 @@ $title_complete=($title_page!=''?$title_page.' - '.$title_base:$title_base);
     <link rel='stylesheet' type='text/css' href='style.css'/>
 </head>
 <body>
-
     
-<a href="index.php?title=Start">Startseite</a> | 
-<a href="suche.php?title=Suche">Suche</a> | 
-<a href="help.php?title=Hilfe">Hilfe</a>  |
-<a href="tests.php?title=Tests">Tests</a>
+<a href="index.php?title=Start" tabindex="-1">Startseite</a> | 
+<a href="suche.php?title=Suche" tabindex="-1">Suche</a> | 
+<a href="help.php?title=Hilfe" tabindex="-1">Hilfe</a>  |
+<a href="tests.php?title=Tests" tabindex="-1">Tests</a>
 
 <hr>
 <?php 
