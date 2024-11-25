c51d85bd3310e2ab7607016fba6011d82b321ac3
birgitreiner <birgitreiner@outlook.de>
2024-11-25 20:30:28 +0100

    Ergänzung GitHub- Link

--- notendb/foot.php
+++ notendb/foot.php
@@ -6,7 +6,9 @@
     <a href="index.php?title=Start" tabindex="-1">Startseite</a> | 
     <a href="suche.php?title=Suche" tabindex="-1">Suche</a> | 
     <a href="help.php?title=Hilfe" tabindex="-1">Hilfe</a>  |
-    <a href="tests.php?title=Tests" tabindex="-1">Tests</a>
+    <a href="tests.php?title=Tests" tabindex="-1">Tests</a> | 
+    <a href="https://github.com/birrei/ndb/tree/main" tabindex="-1" target="_blank">GitHub</a>
+    
 
 </p>  
 <hr>
