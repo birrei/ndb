9f3e0d65efd3e3f442352182be0cd4f3f17b0ca0
birgitreiner <birgitreiner@outlook.de>
2024-12-06 11:38:21 +0100

    Überarbeitung

--- notendb/show_table2.php
+++ notendb/show_table2.php
@@ -2,66 +2,85 @@
 include('head.php');
 
 $object=$_GET['table']; // obligatorisch, Name Tabelle oder View (falls View, Benennung: "v_[tabelle]")
+$edit_table=$object; // default 
 
-// optionale Übergabeparameter
-$edit_table=''; // Tabelle, auf die im Bearbeiten-Link verwiesen werden soll.  
-$sortcol='ID';  // Spalte, nach der sortiert werden soll, default: ID
-$sortorder='ASC'; // Sortier-Reihenfolge, default aufwärts 
+
+/******************************* */
+/** Festlegungen für Bearbeiten-Link */
+
+$add_link_edit=true; // Bearbeiten-Link anzeigen, falls ja: 
 $edit_table_title=''; // Titel der Seite, die über den Bearbeiten-Link aufgerufen wird 
-$add_link_edit=true; // Bearbeiten-Link anzeigen 
 $edit_link_show_newpage=false; // true: Das öffnen des Bearbeiten-Links soll in einem neuen Fenster erfolge
 $add_link_show=false;  // true, falls eine zusätzliche Spalte "Anzeigen" ergänzt werden soll
-$tables_exclude_insertlink=array('musikstueck','satz', 'lookup');
-$tables_use_insertfile=array('lookup'); // aktuell nicht genutzt, Stand 15.07.2024
-$show_filter=false; // falls ein Filter-Box angezeigt werden soll
-$show_insert_link=true; // LInk "Neuen Datensatz einfügen" oben anzeigen 
 
-/*************** */
-
-
-if (!in_array($edit_table,$tables_exclude_insertlink)) {
-  $show_insert_link=false; 
+/* Ausnahme Tabellen-Views */
+if (substr($object,0,2)=='v_') {
+  $edit_table=substr($object,2, strlen($object)-2);     // bei Views (suffix v_): "v_" vorne abschneiden
 }
 
-// Falls übergebenes Objekt = Info-View
-if (!strpos($object, 'info')===false) {
+/*  Ausnahme Info-Views  */
+if (substr($object,0,3)=='v2_') {
   $add_link_edit=false; 
-  $show_insert_link=false; 
 }
 
-if (isset($_GET['edit_table'])) {
-  $edit_table=$_GET['edit_table']; 
-} else {
-  $edit_table=$object; 
-  if (substr($object,0,2)=='v_') {
-    $edit_table=substr($object,2, strlen($object)-2);     // bei Views (suffix v_): "v_" vorne abschneiden
-  }
+/* Ausnahme Test-Views  */
+if (substr($object,0,3)=='v3_') {
+  $add_link_edit=false; 
 }
+
+
 if (isset($_GET['title'])) {
   $edit_table_title=$_GET['title']; 
 }
-if (isset($_GET['add_link_show'])) {
-  $add_link_show=true; 
-}
-if (isset($_GET['show_filter'])) {
-  $show_filter=true; 
-}
-if (isset($_GET['sortcol'])) {
-  $sortcol=$_GET['sortcol'];
-} 
-if (isset($_GET['sortorder'])) {
-    $sortorder=$_GET['sortorder'];
-}
+
 if (isset($_GET['edit_link_show_newpage'])) {
   $edit_link_show_newpage=true; 
 }
 
-echo '<h3>'.$edit_table_title.'</h3>'.PHP_EOL; 
+/******************************* */
+/** Festlegungen für Anzeige "Neu einfügen" - Link (Seitenkopf) */
+
+$show_insert_link=true; // default 
+
+if (in_array($edit_table, array('musikstueck','satz', 'lookup'))) {
+  $show_insert_link=false;
+}
+
+/*  Info-Views  */
+if (substr($object,0,3)=='v2_') {
+  $show_insert_link=false;
+}
+
+/*  Test-Views  */
+if (substr($object,0,3)=='v3_') {
+  $show_insert_link=false;  
+}
+
+/********************************/
+/** Anzeige Link "Anzeigen" in zusäztlicher Spalte der Ergebnistabelle */
+$add_link_show=false; 
+if (isset($_GET['add_link_show'])) {
+  $add_link_show=true; 
+}
+
+/******************************* */
+/** Soll Filter-Möglichkeit angezeigt werden?  */
+
+$show_filter=false; // falls ein Filter-Box angezeigt werden soll
+
+if (isset($_GET['show_filter'])) {
+  $show_filter=true; 
+}
+
+/********************************/
 
 $query = 'SELECT * FROM '.$object.' WHERE 1=1 ';
 
+// echo '<h3>'.$edit_table_title.'</h3>'.PHP_EOL; 
 
-/*********** Filter ********************/
+
+
+/*********** Filter-Anzeige, Query Filter  ********************/
 
 if($show_filter) {
 
@@ -104,11 +123,18 @@ if($show_filter) {
   echo '<p></p>'; 
 }
 
-/*******************************/
+/******** Query Sortierung ***********************/
+$sortcol=(isset($_GET['sortcol'])?$_GET['sortcol']:'ID'); 
+$sortorder=(isset($_GET['sortorder'])?$_GET['sortorder']:'ASC'); 
 
-$query.= ($sortcol!='' ?' ORDER BY '.$sortcol.' '.PHP_EOL.$sortorder:''); 
+$query.= ' ORDER BY '.$sortcol.' '.$sortorder.PHP_EOL; 
 
 // echo '<pre>'.$query.'</pre>'; // Test 
+
+
+
+
+/******************************* */
 
 include_once("dbconn/cl_db.php");
 $conn = new DbConn(); 
@@ -124,7 +150,7 @@ try {
   
   // Link für Neu-Erfassung anzeigen? 
   if ($show_insert_link) {
-    echo '<p><a href="insert_'.$edit_table.'.php?title='.$edit_table_title.'&option=insert">Neu erfassen</a></p>';
+    echo '<p><a href="edit_'.$edit_table.'.php?title='.$edit_table_title.'&option=insert" target="_blank">Neu erfassen</a></p>';
   }
 
   $html->add_link_edit= $add_link_edit; 
@@ -139,6 +165,18 @@ catch (PDOException $e) {
   $info->print_user_error(); 
   $info->print_error($select, $e); 
 }
+
+
+
+// if (isset($_GET['edit_table'])) {
+//   $edit_table=$_GET['edit_table']; // XXX gibt es diese Konstellation überhaupt? 
+// } else {
+//   $edit_table=$object; 
+//   if (substr($object,0,2)=='v_') {
+//     $edit_table=substr($object,2, strlen($object)-2);     // bei Views (suffix v_): "v_" vorne abschneiden
+//   }
+// }
+
 
 
 include('foot.php');
