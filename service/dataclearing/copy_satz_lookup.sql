afba6e594dc24aaaea07c7e00b005d0dd3e2f769
birgitreiner <birgitreiner@outlook.de>
2025-02-09 14:06:34 +0100

    Dataclearing: Copy Satz Attribute

---
+++ service/dataclearing/copy_satz_lookup.sql
@@ -0,0 +1,8 @@
+
+
+insert into satz_lookup (SatzID, LookupID) 
+select 1843 as SatzID, LookupID 
+from satz_lookup where SatzID = 1841;
+
+
+
---
+++ service/dataclearing/copy_satz_schueler.sql
@@ -0,0 +1,5 @@
+
+insert into schueler_satz (SchuelerID, SatzID) 
+select SchuelerID, 1844 as SatzID  
+from schueler_satz where SatzID = 1841
+
