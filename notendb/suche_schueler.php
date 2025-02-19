8221aa031a13f9b1a7c645c4be73594895b62ae2
birgitreiner <birgitreiner@outlook.de>
2025-02-19 09:14:22 +0100

    Neue Suche Schüler

---
+++ notendb/suche_schueler.php
@@ -0,0 +1,322 @@
+<?php 
+include('head.php');
+include("dbconn/cl_db.php");
+
+include("cl_html_table.php");    
+include("cl_html_info.php");  
+
+include("cl_instrument_schwierigkeitsgrad.php");  
+
+include("cl_lookup.php");   
+include("cl_lookuptype.php");
+include("cl_linktype.php");
+include("cl_abfrage.php");
+include("cl_schueler.php");
+
+
+$Erprobt=[];  // im Suchfilter ausgewählte Erprobt-Einträge  (IDs) 
+$Schwierigkeitsgrade=[]; // im Suchfilter ausgewählte Schwierigkeitsgrade  (IDs). Instrument/Schwierigkeitsgrad!  
+$Instrumente=[]; // im Suchfilter ausgewählte Instrumente  (IDs) 
+$Stricharten=[];  // im Suchfilter ausgewählte Stricharten  (IDs) 
+$Notenwerte=[]; // im Suchfilter ausgewählte Notenwerte  (IDs) 
+$Uebungen=[]; // im Suchfilter ausgewählte Übung-Einträge  (IDs) 
+
+$Schueler=[]; 
+
+
+$lookup_all_values_selected=[]; // im Suchfilter ausgewählte Besonderheiten-IDs (gesammelt aus allen lookup-types) ?
+
+
+$ErprobtJahr_von=''; 
+$ErprobtJahr_bis=''; 
+
+$suchtext=''; 
+
+/***************************/
+
+
+
+$filterStandorte='';   
+$filterVerlage='';
+$filterLinktypen='';      
+$filterBesetzung=''; 
+$filterVerwendungszweck='';
+$filterKomponisten='';   
+$filterGattungen='';  
+$filterEpochen='';    
+$filterErprobt=''; 
+$filterSchwierigkeitsgrad=''; 
+$filterInstrumente=''; 
+
+$filterLookups=''; 
+$filterSpieldauer='';
+$filterErprobtJahr='';    
+$filterSchueler=''; 
+
+$filterSuchtext='';  
+
+$filter=false; 
+
+$edit_table=''; /* Tabelle, die über Bearbeiten-Links in Ergebnis-Tabelle abrufbar sein soll */
+
+$Suche = new Abfrage();
+
+?>
+
+
+<!-- Button: alle Filter zurücksetzen --> 
+<input type="button" id="btnReset_All" value="Alle Filter zurücksetzen" onclick="Reset_All();" /> 
+<script type="text/javascript">  
+          function Reset_All() {  
+          for(i=0; i<document.forms[0].elements.length; i++){
+            if(document.forms[0].elements[i].type == 'text'){
+              document.forms[0].elements[i].value=""; 
+            }
+            if(document.forms[0].elements[i].type == 'select-one'){
+              if(document.forms[0].elements[i].id!='Ansicht') {
+                document.forms[0].elements[i].selectedIndex = -1;
+              }
+            }   
+            if(document.forms[0].elements[i].type == 'select-multiple'){
+              document.forms[0].elements[i].selectedIndex = -1;
+            }     
+            if(document.forms[0].elements[i].type == 'checkbox'){
+              document.forms[0].elements[i].checked = 0;
+            }                         
+          }
+      }  
+</script> 
+
+&nbsp;
+&nbsp;
+
+<a onclick="hideFilter()" href="#">Filter ein/ausblenden</a>
+<script> 
+      function hideFilter() {
+        // Sichtbarkeit linke Filter-Spalte ein / aus  
+        if (document.getElementById("search-filter").hidden==false)
+        {
+          document.getElementById("search-filter").hidden=true; 
+          document.getElementById("search-page").style.gridTemplateColumns = "auto";
+        } else 
+        {
+          document.getElementById("search-filter").hidden=false;      
+          document.getElementById("search-page").style.gridTemplateColumns = "350pt auto"; 
+        }
+
+      }
+</script>
+
+
+<?php 
+if (isset($_POST['Ansicht'])) {
+  $Ansicht=$_POST["Ansicht"];
+} else {
+  $Ansicht='Schueler'; // default 
+}
+?> 
+
+
+<div class="search-page" id="search-page">
+<div class="search-filter" id="search-filter">
+
+<form id="Suche" action="" method="post">
+
+<!---- Entscheidung Suche speichern ja / nein -----> 
+<input type="checkbox" id="sp" name="SucheSpeichern"><label for="sp">Suche speichern</label> 
+
+<?php 
+/************** Suchtext  **********/  
+
+if (isset($_POST['suchtext'])) {
+  $suchtext = $_POST['suchtext'];  
+  if ($suchtext!='') { 
+      $Suche->Beschreibung.='* Suchtext: '.$suchtext.PHP_EOL; 
+      $filter=true; 
+  }
+}  
+?>
+<!---- Suche starten -----> 
+<p>Suchtext: <br><input type="text" id="suchtext" name="suchtext" size="30px" value="<?php echo $suchtext; ?>" autofocus> 
+<input class="btnSave" type="submit" value="Suchen" class="btnSave" width="100px">
+</P> 
+
+
+<?php
+
+
+  /************* Schüler  ***********/
+  $schueler = new Schueler();
+  $SchuelerID=''; 
+  if (isset($_POST['SchuelerID'])) {
+    if ($_POST['SchuelerID']!='') {
+      $SchuelerID = $_POST['SchuelerID']; 
+      $schueler->ID=  $SchuelerID; 
+      $schueler->load_row(); 
+      $filterSchueler='='.$SchuelerID.' '; 
+      $Suche->Beschreibung.=($SchuelerID!=''?'* Schüler: '.$schueler->Name.PHP_EOL:'');     
+      $filter=true;       
+    }
+  }
+  $schueler->print_select($SchuelerID,'',$schueler->Title);
+
+  ?>
+</form>
+</div> <!-- ende class search-filter --> 
+<div class="search-result" id="search-result">
+<?php
+
+  if ($filter ) {
+    $query=""; 
+    switch ($Ansicht){
+      case 'Schueler': 
+
+        /** XXX noch integtrieren: schueler_satz.Bemerkung, schueler_material.Bemerkung */
+
+        $query.="
+         select 
+          schueler.ID 
+        , schueler.Name
+        , schueler.Bemerkung   
+        , GROUP_CONCAT(DISTINCT 
+                            IF(sm.ID is not NULL, CONCAT('* ', sm.Name, ': ', material.Name), concat('* ', material.Name)) 
+                        order by 
+                            IF(sm.ID is not NULL, CONCAT(sm.Name, ': ', material.Name), material.Name)
+                        SEPARATOR '<br />') as Materialien
+        , GROUP_CONCAT(
+                DISTINCT concat('* ', sammlung.Name, ' / ', musikstueck.Name, 
+                            IF(satz.Name <> '', CONCAT(' / ', satz.Name), '') 
+                )  
+                order by sammlung.Name, musikstueck.Nummer 
+                SEPARATOR '<br />') as Noten 
+
+
+            ";
+        $edit_table='schueler'; 
+
+    }
+
+
+    switch ($Ansicht){
+      case 'Schueler': 
+ 
+          $query.="
+            from schueler 
+                  left join 
+                  schueler_material on schueler_material.SchuelerID = schueler.ID
+                  left join 
+                  material on material.ID = schueler_material.MaterialID 
+                  left join 
+                  sammlung sm on sm.ID=material.SammlungID 
+                  left join 
+                  schueler_satz on schueler_satz.SchuelerID  = schueler.ID 
+                  left join 
+                  satz on satz.ID = schueler_satz.SatzID 
+                  left join 
+                  musikstueck on musikstueck.ID = satz.MusikstueckID
+                  left join 
+                  sammlung on sammlung.ID = musikstueck.SammlungID
+            WHERE 1=1 ". PHP_EOL; 
+                  break; 
+ 
+        }
+
+
+
+     /* WHERE Filter */   
+
+      switch ($Ansicht){
+        case 'Schueler': 
+                
+          $query.=($filterSchueler!=''?' AND schueler.ID='.$SchuelerID.' ' . PHP_EOL:''); 
+
+          if($suchtext!=''){
+            $query.="AND (schueler.Name LIKE '%".$suchtext."%'   
+                    OR schueler.Bemerkung LIKE '%".$suchtext."%' 
+                    OR sammlung.Name LIKE '%".$suchtext."%' 
+                    OR sammlung.Bemerkung LIKE '%".$suchtext."%' 
+                    OR musikstueck.Name LIKE '%".$suchtext."%'  
+                    OR material.Name LIKE '%".$suchtext."%' 
+                    OR material.Bemerkung LIKE '%".$suchtext."%'                     
+                    OR schueler_satz.Bemerkung  LIKE '%".$suchtext."%'  
+                    OR schueler_material.Bemerkung  LIKE '%".$suchtext."%'                                                                      
+                    ) ". PHP_EOL;         
+          }
+          break; 
+
+
+    }
+
+      /* Gruppierung abhängig von Ansicht   */
+      switch ($Ansicht){    
+        case 'Schueler':         
+          $query.="GROUP BY schueler.ID ". PHP_EOL;     
+          break;    
+
+      }
+
+      /* Sortierung abhängig von Ansicht  */
+      switch ($Ansicht){    
+ 
+        case 'Schueler': 
+          $query.="ORDER BY schueler.Name ". PHP_EOL;           
+          break;             
+      }
+
+      // echo '<pre>'.$query.'</pre>'; // Test  
+
+      if (isset($_POST["SucheSpeichern"])) {
+        $timestamp = time();
+        $Suche->Name= 'Suche '.date("d.m.Y - H:i", time()); // Temp. Name, kann später geändert werden
+        $Suche->Abfrage = $query; 
+        $Suche->Tabelle = $edit_table;
+        $Suche->Abfragetyp='Suche';  
+        $Suche->insert_row2(); 
+        echo '<p>Die Suchabfrage wurde gespeichert <br />'; 
+        echo '<a href="show_abfrage.php?ID='.$Suche->ID.'&title=Abfrage" target="_blank">Ergebnis anzeigen</a>
+            | <a href="edit_abfrage.php?ID='.$Suche->ID.'&title=Abfrage&option=edit" target="_blank">Abfrage bearbeiten</a>
+            | <a href="show_table2.php?table=v_abfrage&sortcol=ID&sortorder=DESC&title=Abfragen&add_link_show&show_filter" target="_blank">Übersicht Abfragen</a>         
+            ';
+            
+        } else {
+          // Abfrage nicht speichern, Ergebnis ausgeben   
+          if ($Suche->Beschreibung!='') {
+            echo '<p>Auswahl:</p><pre>'.$Suche->Beschreibung.'</pre>';
+          }
+    
+          include_once("dbconn/cl_db.php");
+          $conn = new DbConn(); 
+          $db=$conn->db; 
+          
+          $select = $db->prepare($query); 
+            
+          try {
+            $select->execute(); 
+            include_once("cl_html_table.php");      
+            $html = new HtmlTable($select); 
+            $html->add_link_edit=true; 
+            $html->edit_link_table=$edit_table; 
+            $html->edit_link_title=$Ansicht; 
+            $html->edit_link_open_newpage=true; 
+            $html->show_row_count=true; 
+            $html->print_table2(); 
+          }
+          catch (PDOException $e) {
+            include_once("cl_html_info.php"); 
+            $info = new HtmlInfo();      
+            $info->print_user_error(); 
+            $info->print_error($select, $e); 
+          }    
+      }
+
+    } // Ende if($filter)
+    else {
+      echo '<p>Es wurde kein Filter gesetzt. </p>'; 
+    }
+ ?>
+</div> <!-- end class search-result -->
+</div> <!-- end class search-page -->
+
+<?php 
+include('foot.php');
+?>
