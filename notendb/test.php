e57ea0f1729c0c15c2fdb980b8458a004eedfad1
birgitreiner <birgitreiner@outlook.de>
2025-05-22 23:26:10 +0200

    Updates

---
+++ notendb/test.php
@@ -0,0 +1,20 @@
+<?php
+$PageTitle='TEST'; 
+include('head.php');
+// include("cl_satz.php");
+include("class.uebung.php");
+// include("cl_html_info.php");
+
+
+?>
+
+<a href="edit_uebung.php?option=insert">insert</a>
+
+
+
+<?php
+
+
+
+include('foot.php');
+?>
