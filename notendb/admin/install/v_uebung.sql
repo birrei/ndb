40cc46c0527b30200972d240bb4c54fd857669be
birgitreiner <birgitreiner@outlook.de>
2025-06-14 09:23:16 +0200

    Inaktive Schüler ausblenden

--- notendb/admin/install/v_uebung.sql
+++ notendb/admin/install/v_uebung.sql
@@ -8,7 +8,7 @@ SELECT
     , uebung.Anzahl
     , uebungtyp.Einheit
     , uebung.Bemerkung
-	  , CONCAT(
+	, CONCAT(
 	        sammlung.Name, ' - ', 
 	          -- musikstueck.Nummer, ' - ', 
 	          musikstueck.Name, ' - Satz Nr. ',  
@@ -17,11 +17,13 @@ SELECT
     , CONCAT(material.Name, ' (' , materialtyp.Name, ') ', sammlung2.Name) as Material 	          
     , uebung.UebungtypID
 FROM  uebung 
+    INNER join schueler on schueler.ID=uebung.SchuelerID
+                    and schueler.Aktiv=1
     left join uebungtyp on uebung.UebungtypID=uebungtyp.ID 
-    left join schueler on schueler.ID=uebung.SchuelerID
     left join satz  on satz.ID=uebung.SatzID 
     left join musikstueck on satz.MusikstueckID = musikstueck.ID
     left JOIN sammlung on sammlung.ID = musikstueck.SammlungID      
     left join material  on material.ID=uebung.MaterialID
     left JOIN materialtyp on materialtyp.ID = material.MaterialtypID      
     left join sammlung as sammlung2  on sammlung2.ID=material.SammlungID
+
