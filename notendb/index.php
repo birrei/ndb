cd2034cc2062d449b94b11065170eae1cb093dba
birgitreiner <birgitreiner@outlook.de>
2025-08-14 13:32:29 +0200

    Umstellung Layout Startseite

--- notendb/index.php
+++ notendb/index.php
@@ -7,7 +7,9 @@ table.start, td.start {
   border-style: solid;
   border-color: lightgray;
   border-collapse: collapse;
-  padding-right: 10px;     
+  padding-right: 10px;  
+  font-size: 13pt;  
+   
 }
 h1 {
     background-color: lightblue; 
@@ -15,99 +17,91 @@ h1 {
 h3 {
     background-color: greenyellow; 
     margin-top: 0px; 
-
 }
+a {
+  text-decoration: none; 
+  color: black; /* Setzt die Linkfarbe auf blau */
+}
+a:hover {
+  text-decoration: underline; 
+  color: blue; /* Setzt die Linkfarbe auf blau */
+}
+
+
+
 </style>
 
 <h1> Notendatenbank   </h1>
 
 <table class="start">
 <tr> 
-    <td class="start"><h3>Sammlungen</h3></td>
+    <td class="start"><h3>Daten</h3></td>
     <td class="start">
-              <a href="show_table2.php?table=v_sammlung&sortcol=ID&sortorder=DESC&show_filter">Sammlungen</a> | 
-              <a href="edit_sammlung.php?title=Sammlung&option=insert">Neu erfassen</a> 
+              <a href="show_table2.php?table=v_sammlung&sortcol=ID&sortorder=DESC&show_filter">Sammlungen</a> (Sammlungen, Musikstücke, Sätze)<br>
+              <!-- <a href="edit_sammlung.php?title=Sammlung&option=insert">Neu erfassen</a>  -->
+
+              <a href="show_table2.php?table=v_material&sortcol=Name">Material</a> <br>    
+            
+            <a href="show_table2.php?table=v_schueler&sortcol=Name">Schüler</a> <br>  
+            <a href="show_table2.php?table=v_uebung&sortcol=ID&sortorder=DESC">Übungen</a> <br>  
             </td>
            </tr>    
 
 
-      <tr> <td class="start"> <h3>Sammlung Stammdaten</h3></td> 
+
+
+      <tr> <td class="start"> <h3>Stammdaten</h3></td> 
       <td class="start">
-            <a href="show_table2.php?table=verlag&sortcol=Name">Verlage</a> |  
-            <a href="show_table2.php?table=standort&sortcol=Name">Standorte</a> | 
-            <a href="show_table2.php?table=linktype&sortcol=Name">Link-Typen</a> 
+            <a href="show_table2.php?table=standort&sortcol=Name">Standorte</a> <br> 
+
+      <a href="show_table2.php?table=verlag&sortcol=Name">Verlage</a> <br>  
+
+
+              <a href="show_table2.php?table=v_komponist&sortcol=Name">Komponisten</a> <br> 
+              <a href="show_table2.php?table=besetzung&sortcol=Name">Besetzungen</a> <br> 
+              <a href="show_table2.php?table=verwendungszweck&sortcol=Name">Verwendungszwecke</a> <br> 
+              <a href="show_table2.php?table=gattung&sortcol=Name">Gattungen</a> <br> 
+              <a href="show_table2.php?table=epoche&sortcol=Name">Epochen</a> <br> 
+
+            <a href="show_table2.php?table=erprobt&sortcol=Name">Erprobt-Eigenschaften</a> <br> 
+            <a href="show_table2.php?table=schwierigkeitsgrad&sortcol=Name">Schwierigkeitsgrade</a> <br> 
+            <a href="show_table2.php?table=instrument&sortcol=Name">Instrumente</a> <br> 
+            <!-- <a href="show_table2.php?table=v_lookup&sortcol=LookupTypeName,Name">Besonderheiten</a> <br>  -->
+            <a href="show_table2.php?table=v_besonderheiten&sortcol=Name">Besonderheiten</a> <br> 
+            <!-- <a href="show_table2.php?table=lookup_type&sortcol=Name">Besonderheit Typen</a>  -->
+            <a href="show_table2.php?table=v_besonderheittypen&sortcol=Name">Besonderheit Typen</a> <br> 
+            <a href="show_table2.php?table=materialtyp&sortcol=Name">Materialtypen</a>  <br>           
+
+            <a href="show_table2.php?table=status&sortcol=Name">Status Ausprägungen</a> (Status Schüler Satz/Material-Zuordnung)<br> 
+
+            <a href="show_table2.php?table=uebungtyp&sortcol=Name">Übung Typen</a> <br> 
+            <a href="show_table2.php?table=linktype&sortcol=Name">Link-Typen</a> <br> 
+            <a href="show_table2.php?table=abfragetyp&sortcol=Name">Abfrage-Typen</a> <br>             
+
         </td>
       </tr>
 
-        <tr> <td class="start"> <h3>Musikstück Stammdaten</h3> </td> 
-      
-        <td class="start">
-              <a href="show_table2.php?table=v_komponist&sortcol=Name">Komponisten</a> | 
-              <a href="show_table2.php?table=besetzung&sortcol=Name">Besetzungen</a> | 
-              <a href="show_table2.php?table=verwendungszweck&sortcol=Name">Verwendungszwecke</a> | 
-              <a href="show_table2.php?table=gattung&sortcol=Name">Gattungen</a> | 
-              <a href="show_table2.php?table=epoche&sortcol=Name">Epochen</a> 
 
-            </td>
-      </tr>
-
-
-
-        <tr> <td class="start"> <h3>Satz Stammdaten</h3> </td>
-        <td class="start">
-            <a href="show_table2.php?table=erprobt&sortcol=Name">Erprobt-Eigenschaften</a> | 
-            <a href="show_table2.php?table=schwierigkeitsgrad&sortcol=Name">Schwierigkeitsgrade</a> | 
-            <a href="show_table2.php?table=instrument&sortcol=Name">Instrumente</a> | 
-            <!-- <a href="show_table2.php?table=v_lookup&sortcol=LookupTypeName,Name">Besonderheiten</a> |  -->
-            <a href="show_table2.php?table=v_besonderheiten&sortcol=Name">Besonderheiten</a> | 
-            <!-- <a href="show_table2.php?table=lookup_type&sortcol=Name">Besonderheit Typen</a>  -->
-            <a href="show_table2.php?table=v_besonderheittypen&sortcol=Name">Besonderheit Typen</a> 
-            </td>
-        </tr>
-
-        <tr> <td class="start"> <h3>Material</h3> </td> 
-        <td class="start">
-            <a href="show_table2.php?table=v_material&sortcol=Name">Material</a> |    
-            <a href="show_table2.php?table=materialtyp&sortcol=Name">Materialtypen</a>
-            </td>
-        </tr>
-
-        <tr> <td class="start"> <h3>Schüler</h3> </td>
-        <td class="start">
-            <a href="show_table2.php?table=v_schueler&sortcol=Name">Schüler</a> |               
-            <a href="show_table2.php?table=status&sortcol=Name">Status</a> | 
-            <a href="show_table2.php?table=v_uebung&sortcol=ID&sortorder=DESC">Übungen</a> |  
-            <a href="show_table2.php?table=uebungtyp&sortcol=Name">Übung Typen</a> 
-            </td>
-
-      </tr>       
-
-
-        <tr> <td class="start"> <h3>Tests und Info-Sichten</h3> </td> 
-        <td class="start">
-            <a href="show_table3.php">Verwendungszweck Planung</a>  <!-- XXX -->          
-            <a href="tests.php?title=Tests">Tests</a> |   
-            <a href="show_table2.php?table=v2_info_Tempobezeichnungen&sortcol=Tempobezeichnung">Verwendete Tempobezeichnungen</a> | 
-            <a href="show_table2.php?table=v2_info_Spieldauern&sortcol=Spieldauer">Verwendete Spieldauern</a> 
-            </td>
+      <tr> <td class="start"> <h3>Abfragen, Auswertungen</h3> </td> 
+      <td class="start">
+      <a href="show_table3.php">Verwendungszweck Planung</a> (Anzeige Gesamt - Spieldauer)  <br>
+      <a href="show_table2.php?table=v2_info_Tempobezeichnungen&sortcol=Tempobezeichnung">Verwendete Tempobezeichnungen</a> <br> 
+      <a href="show_table2.php?table=v2_info_Spieldauern&sortcol=Spieldauer">Verwendete Spieldauern</a> <br> 
+      <a href="tests.php?title=Tests">Vordefinierte Tests</a>   <br> 
+      <a href="show_table2.php?table=v_abfrage&sortcol=Name&add_link_show">Gespeicherte Abfragen</a> (z.B. Abfragen aus Suche)<br>                                     
+      </td>
       
       </tr>        
-   
-
-        <tr> <td class="start"> <h3>Abfragen</h3> </td> 
-        <td class="start">    
-            <a href="show_table2.php?table=v_abfrage&sortcol=Name&add_link_show">Abfragen</a> | 
-            <a href="show_table2.php?table=abfragetyp&sortcol=Name">Abfrage-Typen</a> 
-            </td>
-      </tr>           
+          
 
       <tr> 
           <td class="start"> <h3>Repository</h3> </td> 
           <td class="start">    
-            <a href="https://github.com/birrei/ndb/tree/main" tabindex="-1" target="_blank">GitHub</a> 
-            <a href="https://github.com/birrei/ndb/blob/main/changelog.md" tabindex="-1" target="_blank">Changelog</a>
-            <a href="https://github.com/birrei/ndb/commits/main/" tabindex="-1" target="_blank">Commits</a>
-            </td>
+            <a href="https://github.com/birrei/ndb/tree/main" tabindex="-1" target="_blank">GitHub Startseite</a><br> 
+            <a href="https://github.com/birrei/ndb/blob/main/changelog.md" tabindex="-1" target="_blank">Changelog</a> (Beschreibung neuer Funktionen sowie Fehlerkorrekturen) <br>
+            <a href="https://github.com/birrei/ndb/commits/main/" tabindex="-1" target="_blank">Commits</a> (Vollständige techn. Änderungs-Historie) <br>
+            <a href="https://github.com/birrei/ndb/blob/main/notes.md" tabindex="-1" target="_blank">Notizen</a> (Sammlung von Erklärungen, die (noch) nicht auf der Hilfe-Seite zu finden sind) <br>
+          </td>
       </tr>            
 
 
@@ -115,9 +109,7 @@ h3 {
 
 <?php 
 include_once('foot.php');
-// include_once('class.Log.php'); 
-// $Loginfo = new Log(); 
-// $Loginfo->printUserName(); 
+
  
 ?>
 
