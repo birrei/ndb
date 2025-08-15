f0b6575442ee12ccbf5ceb3968faa1a29d93dfe7
birgitreiner <birgitreiner@outlook.de>
2025-08-15 18:39:31 +0200

    Obsolete Views gelöscht

--- notendb/admin/install/v2_info_Taktarten.sql
+++
@@ -1,6 +0,0 @@
-create or REPLACE view v2_info_Taktarten as 
-select distinct  NULL as ID 
-, Taktart from satz  
-where Taktart is not null 
-and Taktart <> ''
-order by Taktart
--- notendb/admin/install/v2_info_Tonarten.sql
+++
@@ -1,5 +0,0 @@
-create or REPLACE view v2_info_Tonarten as 
-select distinct NULL as ID, Tonart from satz  
-where Tonart is not null 
-and Tonart <> ''
-order by Tonart
