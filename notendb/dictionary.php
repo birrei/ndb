9b7dd9641ef13274bd39a2bcf441602f9f45ae5c
birgitreiner <birgitreiner@outlook.de>
2025-08-16 11:06:21 +0200

    Dictionary - Abruf von Eigenschaften

---
+++ notendb/dictionary.php
@@ -0,0 +1,161 @@
+<?php
+
+$objekte["v_sammlung"] = array(  
+                'viewname' =>"v_sammlung",     
+                'tablename' =>"sammlung", 
+                'printname' =>"Sammlung", 
+                'printname_plural'    =>"Sammlungen" 
+); 
+
+$objekte["v_material"] = array(  
+                'viewname' =>"v_material",         
+                'tablename' =>"material", 
+                'printname' =>"Material", 
+                'printname_plural'    =>"Materialien" 
+); 
+
+$objekte["v_schueler"] = array(  
+                'viewname' =>"v_schueler",         
+                'tablename' =>"schueler", 
+                'printname' =>"Schüler", 
+                'printname_plural'    =>"Schüler" 
+); 
+
+$objekte["v_uebung"] = array(  
+                'viewname' =>"v_uebung",         
+                'tablename' =>"uebung", 
+                'printname' =>"Übung", 
+                'printname_plural'    =>"Übungen" 
+); 
+
+$objekte["v_standort"] = array(  
+                'viewname' =>"v_standort",         
+                'tablename' =>"standort", 
+                'printname' =>"Standort", 
+                'printname_plural'    =>"Standorte" 
+); 
+
+$objekte["v_verlag"] = array(  
+                'viewname' =>"v_verlag",         
+                'tablename' =>"verlag", 
+                'printname' =>"Verlag", 
+                'printname_plural'    =>"Verlage" 
+); 
+
+$objekte["v_verlage"] = array(  
+                'viewname' =>"v_komponisten",         
+                'tablename' =>"komponist", 
+                'printname' =>"Komponist", 
+                'printname_plural'    =>"Komponisten" 
+); 
+$objekte["v_komponist"] = array(  
+                'viewname' =>"v_komponist",         
+                'tablename' =>"komponist", 
+                'printname' =>"Komponist", 
+                'printname_plural'    =>"Komponisten" 
+); 
+$objekte["v_besetzung"] = array(  
+                'viewname' =>"v_besetzung",         
+                'tablename' =>"besetzung", 
+                'printname' =>"Besetzung", 
+                'printname_plural'    =>"Besetzungen" 
+); 
+
+$objekte["v_verwendungszweck"] = array(  
+                'viewname' =>"v_verwendungszweck",         
+                'tablename' =>"verwendungszweck", 
+                'printname' =>"Verwendungszweck", 
+                'printname_plural'    =>"Verwendungszwecke" 
+); 
+$objekte["v_gattung"] = array(  
+                'viewname' =>"v_gattung",         
+                'tablename' =>"gattung", 
+                'printname' =>"Gattung", 
+                'printname_plural'    =>"Gattungen" 
+); 
+
+$objekte["v_epoche"] = array(  
+                'viewname' =>"v_epoche",         
+                'tablename' =>"epoche", 
+                'printname' =>"Epoche", 
+                'printname_plural'    =>"Epochen" 
+); 
+$objekte["v_erprobt"] = array(  
+                'viewname' =>"v_erprobt",         
+                'tablename' =>"erprobt", 
+                'printname' =>"Erprobt-Eigenschaft", 
+                'printname_plural'    =>"Erprobt-Eigenschaften" 
+); 
+$objekte["v_erprobt"] = array(  
+                'viewname' =>"v_erprobt",         
+                'tablename' =>"erprobt", 
+                'printname' =>"Erprobt-Eigenschaft", 
+                'printname_plural'    =>"Erprobt-Eigenschaften" 
+); 
+$objekte["v_schwierigkeitsgrad"] = array(  
+                'viewname' =>"v_schwierigkeitsgrad",         
+                'tablename' =>"schwierigkeitsgrad", 
+                'printname' =>"Schwierigkeitsgrad", 
+                'printname_plural'    =>"Schwierigkeitsgrade" 
+); 
+
+$objekte["v_instrument"] = array(  
+                'viewname' =>"v_instrument",         
+                'tablename' =>"instrument", 
+                'printname' =>"Instrument", 
+                'printname_plural'    =>"Instrumente" 
+); 
+
+$objekte["v_lookup"] = array(  
+                'viewname' =>"v_lookup",         
+                'tablename' =>"lookup", 
+                'printname' =>"Besonderheit", 
+                'printname_plural'    =>"Besonderheiten" 
+); 
+
+$objekte["v_lookup_type"] = array(  
+                'viewname' =>"v_lookup_type",         
+                'tablename' =>"lookup_type", 
+                'printname' =>"Besonderheit Typ", 
+                'printname_plural'    =>"Besonderheit Typen" 
+); 
+
+$objekte["v_materialtyp"] = array(  
+                'viewname' =>"v_materialtyp",         
+                'tablename' =>"materialtyp", 
+                'printname' =>"Materialtyp", 
+                'printname_plural'    =>"Materialtypen" 
+); 
+
+$objekte["v_status"] = array(  
+                'viewname' =>"v_status",         
+                'tablename' =>"status", 
+                'printname' =>"Status", 
+                'printname_plural'    =>"Status Ausprägungen" 
+); 
+
+$objekte["v_uebungtyp"] = array(  
+                'viewname' =>"v_uebungtyp",         
+                'tablename' =>"uebungtyp", 
+                'printname' =>"Übung Typ", 
+                'printname_plural'    =>"Übung Typen" 
+); 
+$objekte["v_linktype"] = array(  
+                'viewname' =>"v_linktype",         
+                'tablename' =>"linktype", 
+                'printname' =>"Link-Typ", 
+                'printname_plural'    =>"Link-Typen" 
+); 
+
+$objekte["v_abfragetyp"] = array(  
+                'viewname' =>"v_abfragetyp",         
+                'tablename' =>"abfragetyp", 
+                'printname' =>"Abfrage-Typ", 
+                'printname_plural'    =>"Abfrage-Typen" 
+); 
+
+
+
+
+;
+?>
