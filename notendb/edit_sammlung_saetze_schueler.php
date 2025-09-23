1f1898c3ce23ad9040e2daff2e7ecca71f69b59c
birgitreiner <birgitreiner@outlook.de>
2025-09-23 20:25:55 +0200

    Ergänzung Übersicht "Sätze + Schüler"

---
+++ notendb/edit_sammlung_saetze_schueler.php
@@ -0,0 +1,11 @@
+
+<?php 
+include_once('head_raw.php');
+include_once("classes/class.sammlung.php");
+
+$sammlung=new Sammlung(); 
+$sammlung->ID=$_GET["SammlungID"];  
+$sammlung->print_table_saetze_schueler();
+
+include_once('foot_raw.php');
+?>
