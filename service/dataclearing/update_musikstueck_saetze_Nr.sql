401975aae4a5c778e2288a29898ce65085ebd197
birgitreiner <birgitreiner@outlook.de>
2025-09-29 17:14:17 +0200

    Dataclearing:  Satz update Nr via Seitenzahl aus Name

--- service/dataclearing/update_musikstueck_saetze_Nr.sql
+++ service/dataclearing/update_musikstueck_saetze_Nr.sql
@@ -1,7 +1,6 @@
 
 /* Satz Nr anhand Seitenzahl (sofern in Name aufgeführt) updaten */
 
-
 UPDATE satz SET Nr = SUBSTR(Name, INSTR(Name, 'S.') + 3, 2)  where MusikstueckID=2143;
 
 select ID, Nr, Name, SUBSTR(Name, INSTR(Name, 'S.') + 3, 2) Seite from satz where MusikstueckID=2143;
