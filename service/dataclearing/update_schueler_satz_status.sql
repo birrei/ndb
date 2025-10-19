1c803ad464c65a863a21bcb21be950465d3d2c72
birgitreiner <birgitreiner@outlook.de>
2025-10-19 11:59:04 +0200

    Datenkorektur Schüler Status

--- service/dataclearing/update_schueler_satz_status.sql
+++ service/dataclearing/update_schueler_satz_status.sql
@@ -1,3 +1,9 @@
 UPDATE schueler_satz 
 SET StatusID = 1  -- 01 Idee 
 WHERE StatusID IS NULL 
+
+
+--- Update StatusID für einen Satz 
+update schueler_satz set StatusID=1 where SatzID=2843; 
+select * from status order by Name; 
+select * from schueler_satz where SatzID=2843;
