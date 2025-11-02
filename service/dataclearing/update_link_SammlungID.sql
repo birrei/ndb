96dbfee24500cb97bea8b3aa2fbc4d5e38065303
birgitreiner <birgitreiner@outlook.de>
2025-11-02 20:48:18 +0100

    Datenkorrektur

---
+++ service/dataclearing/update_link_SammlungID.sql
@@ -0,0 +1,8 @@
+-- 20:46 02.11.2025
+
+select * from link where SammlungID=571 -- zu 561   
+select * from link where SammlungID=572 -- zu 563   
+
+update link set SammlungID = 561 where SammlungID=571
+update link set SammlungID = 563 where SammlungID=572  
+
