3d69b2a484988b4133a79ec8beb162c7c1dbbe86
birgitreiner <birgitreiner@outlook.de>
2025-12-07 22:25:01 +0100

    (aufräumen)

--- dev/features/20251118_uebung_lookup.sql
+++ dev/features/20251118_uebung_lookup.sql
@@ -1,6 +1,6 @@
 -- Übung, Auswahl Besonderheiten 
 
-drop TABLE IF EXISTS uebung_lookup
+DROP TABLE IF EXISTS uebung_lookup
 ; 
 CREATE TABLE IF NOT EXISTS uebung_lookup (
     `ID` int NOT NULL AUTO_INCREMENT     
@@ -27,8 +27,49 @@ ALTER TABLE uebung_lookup
     ON DELETE RESTRICT ON UPDATE RESTRICT
     ;
 
-
-
 insert into relation (Name) values('uebung');
 
 select * from relation
+
+/*
+
+#Doku: Welche Besonderheiten können ausgewählt werden? 
+
+Zuordnung von Besonderheiten über Checkliste 
+- Variante 1: Freie Auswahl 
+- Variante 2: mit Vorlag: Besonderheiten aus dem Satz, welcher der Übung bereits zugeordet wurde 
+
+Filter: Variante (Optionen radio )
+
+Besonderheiten aus Noten(Material) und solche aus Übungen  (konkret geübt)
+
+--------------
+class.uebung.php: 
+    - print_table_lookups()
+    - add_lookup()
+    - delete_lookup()
+    - delete_lookups()
+    - copy_lookups()
+    -> copy() -> Zeile copy_lookups()
+    -> delete() -> Zeile delete_lookups()
+
+Anpassung edit_uebung_lookup.php 
+Anpassung edit_uebung_lookups.php 
+
+Anpassung edit_uebung.php: iframe > edit_uebung_lookups.php 
+
+class.schueler.php -> print_table_uebungen() (Ergänzung Spalte Besonderheiten)
+
+-------------------------
+
+
+Funktion "Löschen": Sammlung, Musikstück, Satz: Löschung verweigern, wenn mit Satz verknüpfte Übungen vorhanden sind
+class.sammlung.php, class.musikstueck.php, class.satz.php: is_deletable() 
+
+Schüler x Satz: Kombi darf nicht löschbar sein, wenn schon Schüler x Übung x Satz-Verknüpfung vorhanden ist. 
+  
+
+
+
+
+*/
