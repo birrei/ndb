4c2face72c73fb8a9699c878cf54f6521921e4b9
BIRGIT REINER <birgit5005@gmail.com>
2026-03-30 21:48:06 +0200

    Datenkorrektur: Führende Ziffern aus Feld uebung Inhalt entfernen

--- dev/features/20260210_uebung_reihenfolge.sql
+++ dev/features/20260210_uebung_reihenfolge.sql
@@ -1,11 +1,9 @@
 
 ALTER TABLE uebung DROP COLUMN IF EXISTS Reihenfolge; 
 
-
 ALTER TABLE uebung ADD Reihenfolge TINYINT DEFAULT 0 ;   
 
-
-SELECT left(Name,2) from uebung 
+SELECT ID, left(Name,2) from uebung 
 WHERE left(Name,2)  REGEXP '^[0-9]+$' 
 
 
@@ -13,3 +11,16 @@ UPDATE uebung
 SET Reihenfolge = left(Name,2) 
 WHERE left(Name,2)  REGEXP '^[0-9]+$' 
 
+
+------------
+
+SELECT ID
+	, Name 
+	, LTRIM(REPLACE(Name, LEFT(Name,2), '')) Name_Neu 
+from uebung 
+WHERE left(Name,2)  REGEXP '^[0-9]+$' 
+
+
+update uebung 
+set Name = LTRIM(REPLACE(Name, LEFT(Name,2), ''))
+WHERE left(Name,2)  REGEXP '^[0-9]+$' 
