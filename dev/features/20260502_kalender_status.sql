4384603c6ee9aa32258dc414d12259847fac419d
BIRGIT REINER <birgit5005@gmail.com>
2026-05-12 13:21:47 +0200

    (Tabelle verworfen)

--- dev/features/20260502_kalender_status.sql
+++ dev/features/20260502_kalender_status.sql
@@ -1,19 +1,22 @@
 
-ALTER TABLE kalender DROP COLUMN StatusID 
-; 
+-- Verworfen! 
 
-ALTER TABLE kalender ADD StatusID INT;  
-; 
+-- ALTER TABLE kalender DROP COLUMN StatusID 
+-- ; 
+
+-- ALTER TABLE kalender ADD StatusID INT;  
+-- ; 
 
 
-CREATE TABLE IF NOT EXISTS statuskalender   (
-    ID INT NOT NULL AUTO_INCREMENT 
-    , Name VARCHAR(100) NOT NULL 
-    , PRIMARY KEY (ID)
-)
-; 
+-- CREATE TABLE IF NOT EXISTS statuskalender   (
+--     ID INT NOT NULL AUTO_INCREMENT 
+--     , Name VARCHAR(100) NOT NULL 
+--     , PRIMARY KEY (ID)
+-- )
+-- ; 
 
-ALTER TABLE statuskalender  ADD ts_insert datetime DEFAULT CURRENT_TIMESTAMP; 
+-- ALTER TABLE statuskalender  ADD ts_insert datetime DEFAULT CURRENT_TIMESTAMP; 
 
-ALTER TABLE statuskalender  ADD ts_update datetime ON UPDATE CURRENT_TIMESTAMP; 
+-- ALTER TABLE statuskalender  ADD ts_update datetime ON UPDATE CURRENT_TIMESTAMP; 
 
+drop table statuskalender 
