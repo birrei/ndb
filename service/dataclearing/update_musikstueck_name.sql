9d8995675ff2cbf5601cb2316a08050c500c4432
BIRGIT REINER <birgit5005@gmail.com>
2026-06-01 15:01:23 +0200

    Dataclearing Sammlungs-Kopie

--- service/dataclearing/update_musikstueck_name.sql
+++ service/dataclearing/update_musikstueck_name.sql
@@ -6,3 +6,28 @@ update musikstueck
 set Name = REPLACE(Name, 'Abschnitt ', '')
 where SammlungID = 282 
 and Name LIKE 'Abschnitt%'
+
+
+update musikstueck set Name = REPLACE(Name, 'Open Strings and first Finger', 'Open Strings')
+select * from musikstueck 
+where SammlungID = 1334 
+and Nummer BETWEEN 1 AND 10 
+and Name LIKE 'Open Strings and first Finger:%'
+
+
+update musikstueck set Name = REPLACE(Name, 'Open Strings and first Finger', '1st finger')
+-- select * from musikstueck 
+where SammlungID = 1334 
+and Nummer BETWEEN 12 AND 21  
+and Name LIKE 'Open Strings and first Finger%'
+
+
+
+
+
+
+
+
+
+
+   
