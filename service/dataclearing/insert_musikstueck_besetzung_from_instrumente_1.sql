37f7839e8aa19f55eaa08e6badc06f0a596a84f0
BIRGIT REINER <birgit5005@gmail.com>
2026-08-23 00:39:46 +0200

    Sammlung Nachtrag-Updates

--- service/dataclearing/insert_musikstueck_besetzung_from_instrumente_1.sql
+++ service/dataclearing/insert_musikstueck_besetzung_from_instrumente_1.sql
@@ -90,3 +90,29 @@ order by musikstueck.ID
 where Instrumente='Violine, Violine 2'
 
 
+/* 2 Violinen + Klavier  */
+insert into musikstueck_besetzung (MusikstueckID, BesetzungID)
+select MusikstueckID, 104 as BesetzungID -- 2 Violinen + Klavier 
+-- select * 
+from (
+	select sammlung.ID as SammlungID 
+		, sammlung.name as SammlungName 
+		, musikstueck.ID as MusikstueckID 
+		, musikstueck.Name as MusikstueckName 
+		, GROUP_CONCAT(instrument.Name  ORDER BY instrument.Name SEPARATOR ', ') Instrumente 
+	-- select count(*)
+	from sammlung 
+	inner join musikstueck on sammlung.ID = musikstueck.SammlungID 
+	inner join satz on satz.MusikstueckID = musikstueck.ID 
+	inner join satz_schwierigkeitsgrad on satz_schwierigkeitsgrad.SatzID = satz.ID 
+	inner join instrument on instrument.ID = satz_schwierigkeitsgrad.InstrumentID 
+	left join musikstueck_besetzung on musikstueck_besetzung.MusikstueckID=musikstueck.ID 
+	where 1=1 
+	and musikstueck_besetzung.ID IS NULL 
+	and musikstueck.SammlungID=567 -- XXX 
+	group by musikstueck.ID 
+	order by musikstueck.ID 
+) as update_base 
+where Instrumente='Klavier, Violine, Violine 2'
+
+
